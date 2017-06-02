47a28ab8f0abd2490b5df515c122498aa5ed3b5e
Paulo Cesar <pcdsjr@gmail.com>
2017-06-01 19:41:02 -0500

    customize login view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception
+
+  # Set layout
+  layout :layout_by_resource
+
+  private
+
+  # Layout per resource_name
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "devise"
+    else
+      "application"
+    end
+  end
 end
