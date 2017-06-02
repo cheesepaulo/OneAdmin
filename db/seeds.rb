42708e371e11f878ecd3fd1e7ae2d461f56d2fb1
Paulo Cesar <pcdsjr@gmail.com>
2017-06-01 19:11:05 -0500

    create Department model and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 puts 'CADASTRANDO Administradores'
-User.create(name: 'Administrator', email: 'admin@admin.com', password: '123456',
+User.create!(name: 'Administrator', email: 'admin@admin.com', password: '123456',
             kind: :full_access)
-
 puts 'ADMINISTRADORES cadastrados com sucesso!'
+
+
+puts 'CADASTRANDO Departamentos'
+departments = ['Informática', 'Jurídico', 'Arquivo', 'Protocolo', 'Recursos Humanos']
+
+departments.each do |department|
+  Department.find_or_create_by!(name: department)
+end
+puts 'DEPARTAMENTOS cadastrados com sucesso!'
